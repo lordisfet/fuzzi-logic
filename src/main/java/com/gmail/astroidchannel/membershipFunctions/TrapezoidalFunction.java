3df7442a3e6c0391fd27f4297e3b266b5ec6d135
lordisfet <143896699+lordisfet@users.noreply.github.com>
2026-01-10 07:42:35 +0200

    add tramezoidal function

--- src/main/java/com/gmail/astroidchannel/membershipFunctions/TrapezoidalFunction.java
+++ src/main/java/com/gmail/astroidchannel/membershipFunctions/TrapezoidalFunction.java
@@ -2,31 +2,28 @@ package com.gmail.astroidchannel.membershipFunctions;
 
 import com.google.common.collect.Range;
 
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
-public class TriangularFunction implements MembershipFunction {
+public class TrapezoidalFunction implements MembershipFunction {
     private double a;
     private double b;
     private double c;
-//    private double height;
-//    private Range<Double> carrier;
-//    private Range<Double> core;
-//    private Set<Range<Double>> spectrum;
-//    private Shape shape; // Опуклість
+    private double d;
 
-    public TriangularFunction(double a, double b, double c) {
+    public TrapezoidalFunction(double a, double b, double c, double d) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.d = d;
     }
 
-    public TriangularFunction(TriangularFunction other) {
+    public TrapezoidalFunction(TrapezoidalFunction other) {
         this.a = other.a;
         this.b = other.b;
         this.c = other.c;
+        this.d = other.d;
     }
 
     public double getA() {
@@ -53,19 +50,30 @@ public class TriangularFunction implements MembershipFunction {
         this.c = c;
     }
 
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
     @Override
     public double calculate(double x) {
         if (Double.compare(x, a) <= 0) {
             return 0;
         }
-        if (Double.compare(x, c) >= 0) {
+        if (Double.compare(x, d) >= 0) {
             return 0;
+        }
+        if (Double.compare(x, b) >= 0 && Double.compare(x,c) <= 0) {
+            return 1;
         }
         if (Double.compare(x, a) > 0 && Double.compare(x, b) <= 0) {
             return (x - a) / (b - a);
         }
-        if (Double.compare(x, b) > 0 && Double.compare(x, c) <= 0) {
-            return (c - x) / (c - b);
+        if (Double.compare(x, c) > 0 && Double.compare(x, d) <= 0) {
+            return (d - x) / (d - c);
         }
 
         throw new IllegalArgumentException("x = " + x + " is not in conditions");
@@ -78,19 +86,19 @@ public class TriangularFunction implements MembershipFunction {
 
     @Override
     public Range<Double> findCarrier() {
-        return Range.open(a, c);
+        return Range.open(a, d);
     }
 
     @Override
     public Range<Double> findCore() {
-        return Range.closed(b, b);
+        return Range.closed(b, c);
     }
 
     @Override
     public Set<Range<Double>> findSpectrum() {
         Set<Range<Double>> boundaries = new LinkedHashSet<>();
         boundaries.add(Range.open(a,b));
-        boundaries.add(Range.open(b,c));
+        boundaries.add(Range.open(b,d));
 
         return boundaries;
     }
@@ -98,21 +106,22 @@ public class TriangularFunction implements MembershipFunction {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        TriangularFunction that = (TriangularFunction) o;
-        return Double.compare(a, that.a) == 0 && Double.compare(b, that.b) == 0 && Double.compare(c, that.c) == 0;
+        TrapezoidalFunction that = (TrapezoidalFunction) o;
+        return Double.compare(a, that.a) == 0 && Double.compare(b, that.b) == 0 && Double.compare(c, that.c) == 0 && Double.compare(d, that.d) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(a, b, c);
+        return Objects.hash(a, b, c, d);
     }
 
     @Override
     public String toString() {
-        return "TriangularFunction{" +
+        return "TrapezoidalFunction{" +
                 "a=" + a +
                 ", b=" + b +
                 ", c=" + c +
+                ", d=" + d +
                 '}';
     }
 }
