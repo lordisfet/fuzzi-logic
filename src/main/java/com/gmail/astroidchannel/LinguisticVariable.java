f38dc9d99052ba319b25cc44fa0de5c256cd7124
lordisfet <143896699+lordisfet@users.noreply.github.com>
2026-01-14 06:37:51 +0200

    Replace Problem to LingusticVariable

---
+++ src/main/java/com/gmail/astroidchannel/LinguisticVariable.java
@@ -0,0 +1,107 @@
+package com.gmail.astroidchannel;
+
+import com.gmail.astroidchannel.membershipFunctions.MembershipFunction;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class LinguisticVariable {
+    public final static double EPS = 0.00001;
+    private String name;
+    private double leftBorder;
+    private double rightBorder;
+    private Map<String, MembershipFunction> terms = new HashMap<>();
+
+    public LinguisticVariable(String name, double leftBorder, double rightBorder) {
+        this.name = name;
+        this.leftBorder = leftBorder;
+        this.rightBorder = rightBorder;
+    }
+
+    public LinguisticVariable(String name, double leftBorder, double rightBorder, Map<String, MembershipFunction> terms) {
+        this.name = name;
+        this.leftBorder = leftBorder;
+        this.rightBorder = rightBorder;
+        this.terms = terms;
+    }
+
+    public LinguisticVariable(LinguisticVariable other) {
+        this.name = other.name;
+        this.leftBorder = other.leftBorder;
+        this.rightBorder = other.rightBorder;
+        this.terms = new HashMap<>(other.terms);
+    }
+
+    public double getLeftBorder() {
+        return leftBorder;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLeftBorder(double leftBorder) {
+        this.leftBorder = leftBorder;
+    }
+
+    public double getRightBorder() {
+        return rightBorder;
+    }
+
+    public void setRightBorder(double rightBorder) {
+        this.rightBorder = rightBorder;
+    }
+
+    public Map<String, MembershipFunction> getTerms() {
+        return new HashMap<>(terms);
+    }
+
+    public void setTerms(Map<String, MembershipFunction> terms) {
+        this.terms = terms;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        LinguisticVariable that = (LinguisticVariable) o;
+        return Double.compare(leftBorder, that.leftBorder) == 0 && Double.compare(rightBorder, that.rightBorder) == 0 && Objects.equals(name, that.name) && Objects.equals(terms, that.terms);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, leftBorder, rightBorder, terms);
+    }
+
+    @Override
+    public String toString() {
+        return "LinguisticVariable{" +
+                "name='" + name + '\'' +
+                ", leftBorder=" + leftBorder +
+                ", rightBorder=" + rightBorder +
+                ", terms=" + terms +
+                '}';
+    }
+
+    public void addTerms(String termName, MembershipFunction membershipFunction) {
+        terms.put(termName, membershipFunction);
+    }
+
+    public Map<String, Double> fuzzify(double value) {
+        Map<String, Double> result = new HashMap<>();
+
+        for (Map.Entry<String, MembershipFunction> entry : terms.entrySet()) {
+            double degree = entry.getValue().calculate(value);
+
+            if (degree > EPS) {
+                result.put(entry.getKey(), degree);
+            }
+        }
+
+        return result;
+    }
+}
--- src/main/java/com/gmail/astroidchannel/membershipFunctions/zShaped.java
+++ src/main/java/com/gmail/astroidchannel/membershipFunctions/zShaped.java
@@ -2,10 +2,49 @@ package com.gmail.astroidchannel.membershipFunctions;
 
 import com.google.common.collect.Range;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class zShaped implements MembershipFunction {
+    private double coefficient;
+    private double a;
+    private double b;
 
+    public zShaped(double coefficient, double a, double b) {
+        this.coefficient = coefficient;
+        this.a = a;
+        this.b = b;
+    }
+
+    public zShaped(zShaped other) {
+        this.coefficient = other.coefficient;
+        this.a = other.a;
+        this.b = other.b;
+    }
+
+    public double getCoefficient() {
+        return coefficient;
+    }
+
+    public void setCoefficient(double coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
 
     @Override
     public double calculate(double x) {
@@ -30,5 +69,26 @@ public class zShaped implements MembershipFunction {
     @Override
     public Set<Range<Double>> findSpectrum() {
         return Set.of();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        zShaped zShaped = (zShaped) o;
+        return Double.compare(coefficient, zShaped.coefficient) == 0 && Double.compare(a, zShaped.a) == 0 && Double.compare(b, zShaped.b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coefficient, a, b);
+    }
+
+    @Override
+    public String toString() {
+        return "zShaped{" +
+                "coefficient=" + coefficient +
+                ", a=" + a +
+                ", b=" + b +
+                '}';
     }
 }
